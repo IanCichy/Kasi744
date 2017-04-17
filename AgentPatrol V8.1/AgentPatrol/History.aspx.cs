a97a882e4ec8d89d7b27babcc00a09e9bc2fcaee
IanCichy <chichi2020@live.com>
2017-04-17 16:04:21 -0500

    AgentPatrol V8.1

---
+++ AgentPatrol V8.1/AgentPatrol/History.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AgentPatrol;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+
+public partial class History : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            txtFileName.Focus();
+        }
+        else
+        {
+
+        }
+        sqlRuns.ConnectionString = Connections.Connection().ConnectionString;
+        sqlRuns.CancelSelectOnNullParameter = false;
+    }
+
+    protected void btnReturn_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Cardinals.aspx");
+    }
+
+    protected void btnClearSearch_Click(object sender, EventArgs e)
+    {
+        txtFileName.Text = "";
+        txtMapSize.Text = "";
+        txtAgents.Text = "";
+        txtRegions.Text = "";
+        txtSteps.Text = "";
+        txtLimit.Text = "";
+        ddlAlgorithm.SelectedIndex = 0;
+        txtResults.Text = "";
+        txtResults.CssClass = "historyNoData";
+        txtDate1.Text = "";
+        txtDate2.Text = "";
+    }
+
+    protected void gvRuns_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        if (e.CommandName.Equals("ViewRunHistory"))
+        {
+            GridViewRow gvr = (GridViewRow)((Control)e.CommandSource).NamingContainer;
+            int rowIndex = gvr.RowIndex;
+            var fileID = int.Parse(gvRuns.DataKeys[rowIndex]["ID"].ToString());
+            txtResults.Text = getData(fileID);
+            txtResults.CssClass = "historyData";
+
+        }
+    }
+
+    public String getData(int id)
+    {
+        // Set up update command
+        var command =
+            new SqlCommand("SELECT fileData from RunData where ID=@ID")
+            {
+                CommandType = CommandType.Text,
+                Connection = Connections.Connection()
+            };
+        command.Parameters.Add(new SqlParameter("ID", id));
+        // Execute command
+        command.Connection.Open();
+        String message = (String)command.ExecuteScalar();
+        command.Connection.Close();
+
+        return message;
+    }
+
+    protected void gvRuns_RowCreated(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.Header)
+        {
+            foreach (TableCell tc in e.Row.Cells)
+            {
+                if (tc.HasControls())
+                {
+                    // search for the header link
+                    LinkButton lnk = (LinkButton)tc.Controls[0];
+                    if (lnk != null)
+                    {
+                        // inizialize a new image
+                        System.Web.UI.WebControls.Image img = new System.Web.UI.WebControls.Image();
+                        // setting the dynamically URL of the image
+                        img.ImageUrl = "Assets/Images/" + (gvRuns.SortDirection == SortDirection.Ascending ? "arrow_up" : "arrow_down") + ".png";
+                        img.Width = 16;
+                        img.Height = 8;
+                        // checking if the header link is the user's choice
+                        if (gvRuns.SortExpression == lnk.CommandArgument)
+                        {
+                            // adding a space and the image to the header link
+                            tc.Controls.Add(img);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ AgentPatrol V8.1/AgentPatrol/Tile.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using AgentPatrol;
+using System.Drawing;
+using System.Web.UI.WebControls;
+using System.Collections.Generic;
+
+public class Tile
+{
+    private Button btn;
+    private int type;
+    private Point location;
+    private int score;
+    private bool visited;
+    private LinkButton lnkbtn;
+    private Panel pnl;
+    private Label lbl1;
+    private Label lbl2;
+
+    /*
+     * Creates a new tile with a
+     * type
+     * xlocation
+     * ylocation
+     * and also creates the buttons that are displayed
+     */
+    public Tile(int t, int x, int y)
+    {
+        type = t;
+        location = new Point(x, y);
+        btn = new Button();
+        btn.ID = "" + x + "," + y;
+        score = 0;
+        visited = false;
+    }
+
+    /*
+     * Creates a new tile with a
+     * type
+     * xlocation
+     * ylocation
+     * and also creates the buttons that are displayed
+     * additional string input to modify id
+     */
+    public Tile(int t, int x, int y, string s)
+    {
+        type = t;
+        location = new Point(x, y);
+        btn = new Button();
+        btn.ID = s +  x + "," + y;
+    }
+
+    /*
+     * Creates a new tile with a
+     * type
+     * xlocation
+     * ylocation
+     * and also creates the buttons that are displayed
+     * additional string input to modify id
+     * Also a bool to use a panel instead of a button
+     */
+    public Tile(int t, int x, int y, string s, bool b)
+    {
+        if (b)
+        {
+            type = t;
+            location = new Point(x, y);
+            lnkbtn = new LinkButton();
+        }
+        else
+        {
+            type = t;
+            location = new Point(x, y);
+            pnl = new Panel();
+            lbl1 = new Label();
+            lbl2 = new Label();
+        }
+    }
+
+    public void setSize(int x, int y)
+    {
+        if (btn != null)
+        {
+            btn.Width = x;
+            btn.Height = y;
+        }
+        else if (lnkbtn != null)
+        {
+            lnkbtn.Width = x;
+            lnkbtn.Height = y;
+        }
+        else
+        {
+            pnl.Width = x;
+            pnl.Height = y;
+        }
+    }
+
+    public void setType(int t)
+    {
+        type = t;
+    }
+
+    public void setScore(int s)
+    {
+        score = s;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public Tile getTile()
+    {
+        return this;
+    }
+
+    public int getxPos()
+    {
+        return location.X;
+    }
+
+    public int getyPos()
+    {
+        return location.Y;
+    }
+
+    public Point getPointLocation()
+    {
+        return location;
+    }
+
+    public Button getDisplay()
+    {
+        return btn;
+    }
+
+    public LinkButton getDisplayLnkBtn()
+    {
+        return lnkbtn;
+    }
+
+    public Panel getDisplayPnl()
+    {
+        return pnl;
+    }
+
+    public Label getLocLabel()
+    {
+        return lbl1;
+    }
+
+    public Label getAgentLabel()
+    {
+        return lbl2;
+    }
+
+    public int getType()
+    {
+        return type;
+    }
+
+    public Tile Clone()
+    {
+        return new Tile(this.getType(), this.getxPos(), this.getyPos());
+    }
+
+    public bool beenVisited()
+    {
+        return visited;
+    }
+
+    public void setVisited(bool b)
+    {
+        visited = b;
+    }
+}
